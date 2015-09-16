1535fc7aa8870c4e8ade3995c621a3f979397be1
Austin Ezell <austin.r.ezell@gmail.com>
2015-09-16 15:34:18 -0700

    Users allowed to authorize with Twitter

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-let hot = "HOT";
-console.log(`ES2015 is ${hot}`);
+
+
+let authorizeGuests =() =>{
+
+  if(localStorage["firebase:session::twitteam"]){
+    return;
+  }
+
+  let ref = new Firebase("https://twitteam.firebaseio.com");
+  ref.authWithOAuthPopup("twitter", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      alert("Authenticated successfully with payload:", authData);
+    }
+  });
+}
+
+authorizeGuests()
